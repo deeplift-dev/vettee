68b46914c659479b0f827b901fa07ad80f0a1853
Tom Scerri <hello@deeplift.dev>
2024-04-25 16:10:04 +1000

    Add api env.

--- apps/expo/src/utils/api.tsx
+++ apps/expo/src/utils/api.tsx
@@ -20,24 +20,24 @@ export { type RouterInputs, type RouterOutputs } from "@acme/api";
  */
 const getBaseUrl = () => {
   /**
-   * Gets the IP address of your host-machine. If it cannot automatically find it,
-   * you'll have to manually set it. NOTE: Port 3000 should work for most but confirm
-   * you don't have anything else running on it, or you'd have to change it.
-   *
-   * **NOTE**: This is only for development. In production, you'll want to set the
-   * baseUrl to your production API URL.
+   * Returns the API URL based on the environment.
+   * In development, it uses the localhost address.
+   * In production, it uses the API_URL from the environment variables.
    */
 
-  const debuggerHost = Constants.expoConfig?.hostUri;
-  const localhost = debuggerHost?.split(":")[0];
+  if (process.env.NODE_ENV === "production") {
+    return process.env.EXPO_PUBLIC_API_URL;
+  } else {
+    const debuggerHost = Constants.expoConfig?.hostUri;
+    const localhost = debuggerHost?.split(":")[0];
 
-  if (!localhost) {
-    // return "https://turbo.t3.gg";
-    throw new Error(
-      "Failed to get localhost. Please point to your production server.",
-    );
+    if (!localhost) {
+      throw new Error(
+        "Failed to get localhost. Please point to your production server.",
+      );
+    }
+    return `http://${localhost}:3000`;
   }
-  return `http://${localhost}:3000`;
 };
 
 /**
