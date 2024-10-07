2723deb47e842de68c172e40660ff0933e01deaf
Tom Scerri <hello@deeplift.dev>
2024-10-07 20:36:12 +1100

    feat: Update Supabase API keys and add service key

--- apps/nextjs/src/app/privacy/page.tsx
+++ apps/nextjs/src/app/privacy/page.tsx
@@ -1,32 +1,43 @@
 import Link from "next/link";
+
 import LogoText from "~/ui/logo-text";
 
 export default function PrivacyPolicyPage() {
   return (
-    <div className="flex flex-col min-h-screen">
-      <header className="px-4 lg:px-6 h-14 flex items-center">
+    <div className="flex min-h-screen flex-col">
+      <header className="flex h-14 items-center px-4 lg:px-6">
         <Link className="flex items-center justify-center" href="/">
           <LogoText />
         </Link>
       </header>
-      <main className="flex-1">
+      <main className="flex-1 px-2 lg:px-0">
         <section className="w-full py-12">
-         <div className="text-center text-3xl font-bold mb-4">
-          Privacy Policy
+          <div className="mb-4 text-center text-3xl font-bold">
+            Privacy Policy
           </div>
-          <div className="text-center text-gray-500 max-w-[600px] mx-auto">
-            Your privacy is important to us. This privacy policy document outlines the types of personal information that is received and collected by Vettee and how it is used.
+          <div className="mx-auto max-w-[600px] text-center text-gray-500">
+            Your privacy is important to us. This privacy policy document
+            outlines the types of personal information that is received and
+            collected by Vettee and how it is used.
           </div>
-          <div className="text-center text-gray-400 text-sm mt-8">Last updated: 7th October 2024</div>
+          <div className="mt-8 text-center text-sm text-gray-400">
+            Last updated: 7th October 2024
+          </div>
         </section>
-        <article className="prose lg:prose-xl max-w-3xl mx-auto pb-24">
+        <article className="prose lg:prose-xl mx-auto max-w-3xl pb-24">
           <p>
-            Vetttee (“we”, “us”, or “our”) is committed to protecting and respecting your privacy. This Privacy Policy outlines how we collect, use, disclose, and safeguard your information when you use the Vetttee mobile application (“App”). Please read this Privacy Policy carefully. If you do not agree with the terms of this Privacy Policy, please do not use the App.
+            Vettee (“we”, “us”, or “our”) is committed to protecting and
+            respecting your privacy. This Privacy Policy outlines how we
+            collect, use, disclose, and safeguard your information when you use
+            the Vettee mobile application (“App”). Please read this Privacy
+            Policy carefully. If you do not agree with the terms of this Privacy
+            Policy, please do not use the App.
           </p>
           <h2 className="mt-4">1. Information We Collect</h2>
           <h3>a. Personal Information</h3>
           <p>
-            We may collect personal information that you voluntarily provide to us when you register or use the App, including but not limited to:
+            We may collect personal information that you voluntarily provide to
+            us when you register or use the App, including but not limited to:
           </p>
           <ul>
             <li>Name</li>
@@ -37,77 +48,120 @@ export default function PrivacyPolicyPage() {
           </ul>
           <h3>b. Usage Data</h3>
           <p>
-            When you access or use the App, we may automatically collect certain data, including but not limited to:
+            When you access or use the App, we may automatically collect certain
+            data, including but not limited to:
           </p>
           <ul>
-            <li>Device Information (e.g., IP address, operating system, browser type)</li>
-            <li>Usage Information (e.g., pages visited, time spent on each page)</li>
+            <li>
+              Device Information (e.g., IP address, operating system, browser
+              type)
+            </li>
+            <li>
+              Usage Information (e.g., pages visited, time spent on each page)
+            </li>
             <li>Geolocation Data (only if you have given permission)</li>
           </ul>
           <h3>c. Cookies and Tracking Technologies</h3>
           <p>
-            We may use cookies, beacons, and similar tracking technologies to collect and store information. You can control your cookie settings through your device or browser settings.
+            We may use cookies, beacons, and similar tracking technologies to
+            collect and store information. You can control your cookie settings
+            through your device or browser settings.
           </p>
           <h2 className="mt-4">2. How We Use Your Information</h2>
-          <p>
-            We use the information we collect in the following ways:
-          </p>
+          <p>We use the information we collect in the following ways:</p>
           <ul>
             <li>To provide, maintain, and improve the App’s functionality.</li>
             <li>To process your registration and manage your account.</li>
-            <li>To send administrative information, such as updates and security alerts.</li>
-            <li>To provide personalized user experiences and recommendations.</li>
-            <li>To respond to your comments, questions, or customer support requests.</li>
+            <li>
+              To send administrative information, such as updates and security
+              alerts.
+            </li>
+            <li>
+              To provide personalized user experiences and recommendations.
+            </li>
+            <li>
+              To respond to your comments, questions, or customer support
+              requests.
+            </li>
             <li>To analyze usage trends and improve the App’s features.</li>
-            <li>To comply with legal obligations and enforce our Terms of Service.</li>
+            <li>
+              To comply with legal obligations and enforce our Terms of Service.
+            </li>
           </ul>
           <h2 className="mt-4">3. Sharing Your Information</h2>
           <p>
-            We do not share your personal information with third parties except in the following circumstances:
+            We do not share your personal information with third parties except
+            in the following circumstances:
           </p>
           <ul>
-            <li>Service Providers: We may share your information with trusted service providers who assist in delivering services to you (e.g., analytics, cloud storage).</li>
-            <li>Legal Obligations: We may disclose your information if required to do so by law or in response to valid legal requests by public authorities.</li>
-            <li>Business Transfers: If we are involved in a merger, acquisition, or asset sale, your information may be transferred as part of that transaction.</li>
+            <li>
+              Service Providers: We may share your information with trusted
+              service providers who assist in delivering services to you (e.g.,
+              analytics, cloud storage).
+            </li>
+            <li>
+              Legal Obligations: We may disclose your information if required to
+              do so by law or in response to valid legal requests by public
+              authorities.
+            </li>
+            <li>
+              Business Transfers: If we are involved in a merger, acquisition,
+              or asset sale, your information may be transferred as part of that
+              transaction.
+            </li>
           </ul>
           <h2 className="mt-4">4. Data Security</h2>
           <p>
-            We implement appropriate technical and organizational measures to safeguard your information from unauthorized access, alteration, disclosure, or destruction. While we strive to protect your personal data, no method of transmission over the internet or electronic storage is completely secure. We cannot guarantee absolute security.
+            We implement appropriate technical and organizational measures to
+            safeguard your information from unauthorized access, alteration,
+            disclosure, or destruction. While we strive to protect your personal
+            data, no method of transmission over the internet or electronic
+            storage is completely secure. We cannot guarantee absolute security.
           </p>
           <h2 className="mt-4">5. Data Retention</h2>
           <p>
-            We retain your personal information only for as long as is necessary for the purposes set out in this Privacy Policy, or as required by law. Once the data is no longer needed, we will delete or anonymize it.
+            We retain your personal information only for as long as is necessary
+            for the purposes set out in this Privacy Policy, or as required by
+            law. Once the data is no longer needed, we will delete or anonymize
+            it.
           </p>
           <h2 className="mt-4">6. Children’s Privacy</h2>
           <p>
-            The App is not intended for use by individuals under the age of 13. We do not knowingly collect personal information from children under 13. If we become aware that we have collected such information, we will take steps to delete it as soon as possible.
+            The App is not intended for use by individuals under the age of 13.
+            We do not knowingly collect personal information from children under
+            13. If we become aware that we have collected such information, we
+            will take steps to delete it as soon as possible.
           </p>
           <h2 className="mt-4">7. Your Rights and Choices</h2>
-          <p>
-            Depending on your jurisdiction, you may have the right to:
-          </p>
+          <p>Depending on your jurisdiction, you may have the right to:</p>
           <ul>
-            <li>Access, correct, update, or request deletion of your personal information.</li>
+            <li>
+              Access, correct, update, or request deletion of your personal
+              information.
+            </li>
             <li>Restrict or object to the processing of your information.</li>
             <li>Withdraw your consent at any time (if applicable).</li>
           </ul>
           <p>
-            To exercise these rights, please contact us using the information provided in the “Contact Us” section below.
+            To exercise these rights, please contact us using the information
+            provided in the “Contact Us” section below.
           </p>
           <h2 className="mt-4">8. Changes to This Privacy Policy</h2>
           <p>
-            We may update this Privacy Policy from time to time. Any changes will be posted within the App, and the “Last Updated” date at the top will reflect the effective date of the updated policy. We encourage you to review this policy periodically for any changes.
+            We may update this Privacy Policy from time to time. Any changes
+            will be posted within the App, and the “Last Updated” date at the
+            top will reflect the effective date of the updated policy. We
+            encourage you to review this policy periodically for any changes.
           </p>
           <h2 className="mt-4">9. Contact Us</h2>
           <p>
-            If you have any questions or concerns about this Privacy Policy or our data practices, please contact us at:
+            If you have any questions or concerns about this Privacy Policy or
+            our data practices, please contact us at:
           </p>
+          <p>Email: info@vettee.vet</p>
           <p>
-            Email: drjesseharvey@gmail.com<br />
-            Address: info@vette.vet
-          </p>
-          <p>
-            By using the Vetttee App, you acknowledge that you have read and understood this Privacy Policy and agree to its terms.
+            By using the Vettee App, you acknowledge that you have read and
+            understood this Privacy Policy and agree to its terms.
           </p>
         </article>
       </main>
--- apps/nextjs/src/app/terms/page.tsx
+++ apps/nextjs/src/app/terms/page.tsx
@@ -1,89 +1,129 @@
 import Link from "next/link";
+
 import LogoText from "~/ui/logo-text";
 
 export default function TermsPage() {
   return (
-    <div className="flex flex-col min-h-screen">
-      <header className="px-4 lg:px-6 h-14 flex items-center">
+    <div className="flex min-h-screen flex-col">
+      <header className="flex h-14 items-center px-4 lg:px-6">
         <Link className="flex items-center justify-center" href="/">
           <LogoText />
         </Link>
       </header>
-      <main className="flex-1">
+      <main className="flex-1 px-2 lg:px-0">
         <section className="w-full py-12">
-         <div className="text-center text-3xl font-bold mb-4">
-          Terms of Service
+          <div className="mb-4 text-center text-3xl font-bold">
+            Terms of Service
           </div>
-          <div className="text-center text-gray-500 max-w-[600px] mx-auto">
-            This covers the terms of use of the Vettee app. If you have any questions, please contact us at info@vettee.vet.
+          <div className="mx-auto max-w-[600px] text-center text-gray-500">
+            This covers the terms of use of the Vettee app. If you have any
+            questions, please contact us at info@vettee.vet.
           </div>
-          <div className="text-center text-gray-400 text-sm mt-8">Last updated: 7th October 2024</div>
+          <div className="mt-8 text-center text-sm text-gray-400">
+            Last updated: 7th October 2024
+          </div>
         </section>
-        <article className="prose lg:prose-xl max-w-3xl mx-auto pb-24">
+        <article className="prose lg:prose-xl mx-auto max-w-3xl pb-24">
           <p>
-            Please read these Terms of Service ("Terms", "ToS") carefully before using the Vettee mobile application (the "Service") operated by [Your Company Name] ("us", "we", or "our").
+            Please read these Terms of Service ("Terms", "ToS") carefully before
+            using the Vettee mobile application (the "Service") operated by
+            [Your Company Name] ("us", "we", or "our").
           </p>
           <h2 className="mt-4">1. Acceptance of Terms</h2>
           <p>
-            By accessing or using the Service, you agree to be bound by these Terms. If you disagree with any part of the terms, then you may not access the Service.
+            By accessing or using the Service, you agree to be bound by these
+            Terms. If you disagree with any part of the terms, then you may not
+            access the Service.
           </p>
           <h2 className="mt-4">2. Description of Service</h2>
           <p>
-            Vettee is an AI-powered application designed to assist pet owners in caring for their pets. The app provides [brief description of key features, e.g., "personalized feeding schedules, exercise recommendations, and health monitoring"].
+            Vettee is an AI-powered application designed to assist pet owners in
+            caring for their pets. The app provides [brief description of key
+            features, e.g., "personalized feeding schedules, exercise
+            recommendations, and health monitoring"].
           </p>
           <h2 className="mt-4">3. User Accounts</h2>
           <h3>3.1. Account Creation</h3>
           <p>
-            You must create an account to use certain features of the Service. You are responsible for maintaining the confidentiality of your account and password.
+            You must create an account to use certain features of the Service.
+            You are responsible for maintaining the confidentiality of your
+            account and password.
           </p>
           <h3>3.2. Accurate Information</h3>
           <p>
-            You agree to provide accurate, current, and complete information during the registration process and to update such information to keep it accurate, current, and complete.
+            You agree to provide accurate, current, and complete information
+            during the registration process and to update such information to
+            keep it accurate, current, and complete.
           </p>
           <h2 className="mt-4">4. User Content</h2>
           <h3>4.1. Responsibility</h3>
           <p>
-            Our Service allows you to post, link, store, share and otherwise make available certain information, text, graphics, videos, or other material ("User Content"). You are responsible for the User Content that you post to the Service.
+            Our Service allows you to post, link, store, share and otherwise
+            make available certain information, text, graphics, videos, or other
+            material ("User Content"). You are responsible for the User Content
+            that you post to the Service.
           </p>
           <h3>4.2. License</h3>
           <p>
-            By posting User Content to the Service, you grant us the right and license to use, modify, publicly perform, publicly display, reproduce, and distribute such User Content on and through the Service.
+            By posting User Content to the Service, you grant us the right and
+            license to use, modify, publicly perform, publicly display,
+            reproduce, and distribute such User Content on and through the
+            Service.
           </p>
           <h2 className="mt-4">5. AI-Generated Content and Recommendations</h2>
           <h3>5.1. Accuracy</h3>
           <p>
-            The Service utilizes artificial intelligence to generate content and recommendations. While we strive for accuracy, we do not guarantee the completeness, reliability, or accuracy of this AI-generated content.
+            The Service utilizes artificial intelligence to generate content and
+            recommendations. While we strive for accuracy, we do not guarantee
+            the completeness, reliability, or accuracy of this AI-generated
+            content.
           </p>
           <h3>5.2. Professional Advice</h3>
           <p>
-            AI-generated recommendations should not replace professional veterinary advice. Always consult with a qualified veterinarian for your pet's health concerns.
+            AI-generated recommendations should not replace professional
+            veterinary advice. Always consult with a qualified veterinarian for
+            your pet's health concerns.
           </p>
           <h2 className="mt-4">6. Privacy Policy</h2>
           <p>
-            Your use of the Service is also governed by our Privacy Policy, which can be found at [link to Privacy Policy].
+            Your use of the Service is also governed by our Privacy Policy,
+            which can be found at [link to Privacy Policy].
           </p>
           <h2 className="mt-4">7. Intellectual Property</h2>
           <p>
-            The Service and its original content (excluding User Content), features, and functionality are and will remain the exclusive property of [Your Company Name] and its licensors.
+            The Service and its original content (excluding User Content),
+            features, and functionality are and will remain the exclusive
+            property of [Your Company Name] and its licensors.
           </p>
           <h2 className="mt-4">8. Termination</h2>
           <p>
-            We may terminate or suspend your account immediately, without prior notice or liability, for any reason whatsoever, including without limitation if you breach the Terms.
+            We may terminate or suspend your account immediately, without prior
+            notice or liability, for any reason whatsoever, including without
+            limitation if you breach the Terms.
           </p>
           <h2 className="mt-4">9. Limitation of Liability</h2>
           <p>
-            In no event shall [Your Company Name], nor its directors, employees, partners, agents, suppliers, or affiliates, be liable for any indirect, incidental, special, consequential or punitive damages, including without limitation, loss of profits, data, use, goodwill, or other intangible losses, resulting from your access to or use of or inability to access or use the Service.
+            In no event shall [Your Company Name], nor its directors, employees,
+            partners, agents, suppliers, or affiliates, be liable for any
+            indirect, incidental, special, consequential or punitive damages,
+            including without limitation, loss of profits, data, use, goodwill,
+            or other intangible losses, resulting from your access to or use of
+            or inability to access or use the Service.
           </p>
           <h2 className="mt-4">10. Changes to Terms</h2>
           <p>
-            We reserve the right, at our sole discretion, to modify or replace these Terms at any time. We will provide notice of any significant changes by posting the new Terms on this page.
+            We reserve the right, at our sole discretion, to modify or replace
+            these Terms at any time. We will provide notice of any significant
+            changes by posting the new Terms on this page.
           </p>
           <h2 className="mt-4">11. Contact Us</h2>
           <p>
-            If you have any questions about these Terms, please contact us at info@vettee.vet.
+            If you have any questions about these Terms, please contact us at
+            info@vettee.vet.
           </p>
           <p>
-            By using the Service, you acknowledge that you have read and understood these Terms and agree to be bound by them.
+            By using the Service, you acknowledge that you have read and
+            understood these Terms and agree to be bound by them.
           </p>
         </article>
       </main>
